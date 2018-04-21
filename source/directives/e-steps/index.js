9948aa46f3a2f1203059701a05a21607778c6084
Tamás Bíró <tamas.biro@emarsys.com>
2018-04-21 11:39:58 +0200

    refactor(e-steps): implement e-icon instead of svgs

--- source/directives/e-steps/index.js
+++ source/directives/e-steps/index.js
@@ -3,16 +3,12 @@
 let templateHtml = `
 <div class="e-steps">
   <div class="e-steps__nav e-steps__nav-prev" ng-click="ctrl.action(ctrl.prev)" ng-class="{'e-steps__nav-disabled': !ctrl.prev}">
-    <svg class="e-icon">
-      <use xlink:href="#caret-left"></use>
-    </svg>
+    <e-icon icon="caret-left"></e-icon>
     <span>{{ 'Previous step' | translate }}</span>
   </div>
   <div class="e-steps__nav e-steps__nav-next" ng-click="ctrl.action(ctrl.next)" ng-class="{'e-steps__nav-disabled': !ctrl.next}">
     <span>{{ 'Next step' | translate }}</span>
-    <svg class="e-icon">
-      <use xlink:href="#caret-right"></use>
-    </svg>
+    <e-icon icon="caret-right"></e-icon>
   </div>
   <div class="e-steps__progress">
     <div class="e-steps__item" ng-repeat="(step, data) in ctrl.steps" ng-bind="data.value | translate" ng-class="{'e-steps__item-current': ctrl.step === step + 1, 'e-steps__item-disabled': data.disabled, 'e-steps__item-action': !data.disabled &amp;&amp; ctrl.step !== step + 1}" ng-click="ctrl.action(step + 1)">
